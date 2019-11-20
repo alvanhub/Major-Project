10e4ecef9b458d3f1e6a8f6a74509dc6ad14633b
alvan alam <alvanalam@gmail.com>
2019-11-20 14:12:54 -0600

    worked on movement system

--- sketch.js
+++ sketch.js
@@ -6,12 +6,17 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid;
-let rows = 30;
-let cols = 30;
+let rows = 50;
+let cols = 50;
 let playerX = 15;
 let playerY = 15;
 let yVelocity = 0;
 let xVelocity = 0;
+let up = false;
+let down = false;
+let right = false;
+let left = false;
+
 
 
 function setup() {
@@ -25,7 +30,9 @@ function draw() {
   background(255);
   displayGrid(grid, rows, cols);
   if (frameCount%10 === 0) {
-    keyPressed();
+    keyReleased();
+    playerMovement();
+    console.log(down);
   }
 }
 
@@ -66,36 +73,64 @@ function windowResized() {
   }
 }
 
-function keyPressed() {
-  grid[playerY][playerX] = 0;
+function keyReleased() {
   
   if (key === "s") {
-    
-      yVelocity = 1;
-      xVelocity = 0;
-    
-   
-  }
-  if (key === "d") {
-    
-      yVelocity = 0;
-      xVelocity = 1;
-    
-  }
-  if (key === "a") {
-      yVelocity = 0;
-      xVelocity = -1;
-  }
-  if (key === "w") {
-      yVelocity = -1;
-      xVelocity = 0;
-  
+    down = !down;
+    left = false;
+    right = false;
+    up = false;
   }
 
+  else if (key === "d") {
+    down = false;
+    left = false;
+    right = !right;
+    up = false;
+    
+  }
+  else if (key === "a") {
+    down = false;
+    left = !left;
+    right = false;
+    up = false;
+  }
+  else if (key === "w") {
+    down = false;
+    left = false;
+    right = false;
+    up = !up;
+  }
+  
+  
+}
+function playerMovement() {
+
+  grid[playerY][playerX] = 0;
+
+  if(down) {
+    yVelocity = 1;
+    xVelocity = 0;
+  }
+  else if(up) {
+    yVelocity = -1;
+    xVelocity = 0;
+  }
+  else if(right) {
+    yVelocity = 0;
+    xVelocity = 1;
+  }
+  else if(left) {
+    yVelocity = 0;
+    xVelocity = -1;
+  }else{
+    yVelocity = 0;
+    xVelocity = 0;
+  }
+  
+  
   playerY += yVelocity;
   playerX += xVelocity;
-
+  
   grid[playerY][playerX] = 1;
-
-}
-
+ }
